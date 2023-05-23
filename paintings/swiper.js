a93c9f3d52a896fb6f22c051b1bd9ad288d5e7f8
Vardgesa <Vardges20032@gmail.com>
2023-05-23 15:08:14 -0400

    pre parallax slider

--- paintings/swiper.js
+++ paintings/swiper.js
@@ -1,17 +1,32 @@
-var swiper = new Swiper(".mySwiper", {
+let  swiper = new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  slidesPerView:4,
-  slidesPerPage:2,
-  spaceBetween: 100,
+  slidesPerView:5,
+  spaceBetween: 30,
   speed: 500,
-  slidesOffsetAfter	:200,
-  slidesOffsetBefore	:50,
-  
-
-
-
-  
+  slidesOffsetAfter	:700,
+  slidesOffsetBefore	:0,
+  fadeEffect: {
+    crossFade: true
+  },
+  keyboard:{
+    enabled:true,
+    onlyInViewport:true,
+    pageUpDown:true,
+  },
+  autoHeight:true,
+  effect:"slide",
+  breakpoints:{
+    400:{
+      slidesPerView:2,
+    },
+    590:{
+      slidesPerView:3,
+    },
+    1000:{
+      slidesPerView:4,
+    }
+  }
 });
