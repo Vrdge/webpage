dc7eff42abce185d8cbe757852ed1501cbebd8e6
Vardgesa <Vardges20032@gmail.com>
2023-05-27 08:52:03 -0400

    Final

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,18 +1,16 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
+function readMore(){
+  let btn_read_more = document.getElementById('button')
+  let read_more = document.querySelectorAll('read-more')
+  let dots = document.getElementById('dots')
+  if(dots.style.display === 'none'){
+    dots.style.display = 'inline'
+    btn_read_more.innerHTML = 'More'
+    $(".read-more").css({ display: "none",});
 
-  });
+  }else{
+    dots.style.display = 'none'
+    btn_read_more.innerHTML = 'Hide'
+    $(".read-more").css({ display: "block",});
+  }
+
+}
